fb90636ec16dcbbaaf75930130875fb62f40ecc6
nikolinaneric <nericnikolina@gmail.com>
2023-03-11 17:21:43 +0100

    heroku deploy

--- youtube_spotify/youtube_spotify/settings.py
+++ youtube_spotify/youtube_spotify/settings.py
@@ -156,7 +156,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
 SPOTIPY_REDIRECT_URI = 'http://localhost:8000/spotify/callback'
-SECRET_KEY = 'secret'
+
 
 
 
